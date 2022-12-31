f470daa59a15fe77c0359c692c676428ac7735f4
Benny Burkert <benny.burkert@codecentric.de>
2022-12-31 14:18:44 +0100

    Weird sized arrays 2

--- plain_c/ant.c
+++ plain_c/ant.c
@@ -6,13 +6,13 @@
 #include <math.h>
 #include "../cities/5/cities5.h"
 
-#define MAX_CITIES 100              // Maximum number of cities
-#define MAX_ANTS 50                  // Maximum number of ants
+#define CITIES 10                    //  number of cities
+#define ANTS 25                      //  number of ants
 #define ALPHA 1.0                    // Pheromone influence
 #define BETA 5.0                     // Distance influence
 #define RHO 0.5                      // Pheromone evaporation coefficient
 #define QVAL 100                     // Pheromone deposit coefficient
-#define INIT_PHER (1.0 / MAX_CITIES) // Initial pheromone level
+#define INIT_PHER (1.0 / CITIES) // Initial pheromone level
 
 // Structure to represent an ant
 typedef struct
@@ -21,8 +21,8 @@ typedef struct
     int next_city;        // Next city to visit
     int start_city;       // Start city
     int path_index;       // Index in the path array
-    int path[MAX_CITIES]; // Visited cities in order
-    int visited[MAX_CITIES]; // Array to keep track of visited cities
+    int path[CITIES]; // Visited cities in order
+    int visited[CITIES]; // Array to keep track of visited cities
     double tour_length;   // Length of current tour
 } Ant;
 
@@ -35,39 +35,27 @@ double distance(City city1, City city2)
 }
 
 int main() {
-    City cities[MAX_CITIES];              // Array of cities
-    Ant ants[MAX_ANTS];                   // Array of ants
-    double phero[MAX_CITIES][MAX_CITIES]; // Pheromone levels on the edges
-    double probs[MAX_CITIES];             // Probabilities of moving to a city
-    int n_cities;                         // Number of cities
-    int n_ants;                           // Number of ants
+    City cities[CITIES];              // Array of cities
+    Ant ants[ANTS];                   // Array of ants
+    double phero[CITIES][CITIES]; // Pheromone levels on the edges
+    double probs[CITIES];             // Probabilities of moving to a city     // Number of ants
     int i, j;                             // Loop indices
 
     // Read in the number of cities and ants
-    printf("Number of Cities: ");
-    scanf("%d", &n_cities);
-    n_ants = 25;
-    printf("number cities=%d, number ants=%d\n", n_cities, n_ants);
+    printf("number cities=%d, number ants=%d\n", CITIES, ANTS);
 
-    switch (n_cities) {
-        case 5:
-            getCities5(cities);
-        case 10:
-            getCities10(cities);
-        default:
-            getCities5(cities);
-    }
+    getCities10(cities);
 
     // Initialize the pheromone levels to the initial value
-    for (i = 0; i < n_cities; i++) {
-        for (j = 0; j < n_cities; j++) {
+    for (i = 0; i < CITIES; i++) {
+        for (j = 0; j < CITIES; j++) {
             phero[i][j] = INIT_PHER;
         }
     }
 
     // Initialize the ants
-    for (i = 0; i < n_ants; i++) {
-        ants[i].cur_city = rand() % n_cities; // Assign a random starting city
+    for (i = 0; i < ANTS; i++) {
+        ants[i].cur_city = rand() % CITIES; // Assign a random starting city
         ants[i].start_city = ants[i].cur_city;
         ants[i].path_index = 0;               // Set the path index to 0
         ants[i].tour_length = 0;              // Set the tour length to 0
@@ -75,13 +63,13 @@ int main() {
 
     // Run the ant algorithm
     int tour_steps = 0;
-    while (tour_steps < n_cities) {
+    while (tour_steps < CITIES) {
         // Have each ant explore the solution space
-        for (i = 0; i < n_ants; i++) {
-            if (ants[i].path_index < n_cities) { // If the ant has not visited all the cities
+        for (i = 0; i < ANTS; i++) {
+            if (ants[i].path_index < CITIES) { // If the ant has not visited all the cities
                 //Calculate denominator to distribute possibility between 0 and 1
                 double denominator = 0.0;
-                for (int k = 0; k < n_cities; k++) {
+                for (int k = 0; k < CITIES; k++) {
                     if (k != ants[i].cur_city &&
                         !ants[i].visited[k]) { // Cannot move to current city or a visited city
                         denominator += pow(phero[ants[i].cur_city][k], ALPHA) *
@@ -89,7 +77,7 @@ int main() {
                     }
                 }
                 // Calculate the probabilities of moving to each city
-                for (j = 0; j < n_cities; j++) {
+                for (j = 0; j < CITIES; j++) {
                     if (j != ants[i].cur_city && !ants[i].visited[j]) { // Cannot move to current city OR already visited city
                         probs[j] = pow(phero[ants[i].cur_city][j], ALPHA) *
                                    pow(1.0 / distance(cities[ants[i].cur_city], cities[j]), BETA) / denominator;
@@ -100,7 +88,7 @@ int main() {
                 // Choose the next city based on the probabilities
                 double r = ((double) rand()) / RAND_MAX;
                 double total = 0.0;
-                for (j = 0; j < n_cities; j++) {
+                for (j = 0; j < CITIES; j++) {
                     total += probs[j];
                     if (total >= r) {
                         ants[i].next_city = j;
@@ -115,16 +103,16 @@ int main() {
             }
         }
         // Update the pheromone levels
-        for (i = 0; i < n_ants; i++) {
-            for (j = 1; j < n_cities; j++) {
+        for (i = 0; i < ANTS; i++) {
+            for (j = 1; j < CITIES; j++) {
                 phero[ants[i].path[j - 1]][ants[i].path[j]] += QVAL / ants[i].tour_length;
             }
-            phero[ants[i].path[n_cities - 1]][ants[i].path[0]] += QVAL / ants[i].tour_length; // Return to starting city
+            phero[ants[i].path[CITIES - 1]][ants[i].path[0]] += QVAL / ants[i].tour_length; // Return to starting city
         }
 
         // Evaporate pheromones
-        for (i = 0; i < n_cities; i++) {
-            for (j = 0; j < n_cities; j++) {
+        for (i = 0; i < CITIES; i++) {
+            for (j = 0; j < CITIES; j++) {
                 phero[i][j] *= (1.0 - RHO);
             }
         }
@@ -133,7 +121,7 @@ int main() {
     // Finished the ant algorithm
 
     // Returning to the start city
-    for (i = 0; i < n_ants; i++)
+    for (i = 0; i < ANTS; i++)
     {
         ants[i].path[ants[i].path_index++] = ants[i].start_city;
         ants[i].tour_length += distance(cities[ants[i].cur_city], cities[ants[i].start_city]);
@@ -142,7 +130,7 @@ int main() {
     // Find the shortest tour
     int min_index = 0;
     double min_length = ants[0].tour_length;
-    for (i = 1; i < n_ants; i++)
+    for (i = 1; i < ANTS; i++)
     {
         if (ants[i].tour_length < min_length)
         {
@@ -152,7 +140,7 @@ int main() {
     }
     // Print the shortest tour
     printf("Shortest tour: ");
-    for (i = 0; i <= n_cities; i++)
+    for (i = 0; i <= CITIES; i++)
     {
         printf("%d ", ants[min_index].path[i]);
     }
