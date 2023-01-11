0151b9b9babe1066f19992c2b6ce7f9515003f5f
Benny Burkert <benny.burkert@codecentric.de>
2023-01-11 19:19:00 +0100

    Minor changes

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#if any statement returns a non-true return value
+set -e
+
 if [ $# -ne 1 ]; then
   echo "Error: Script requires exactly one argument (plain, openmp, opencl)"
   exit 1
@@ -20,8 +23,10 @@ elif [ $1 = "opencl" ]; then
 elif [ $1 = "all" ]; then
   ./run.sh plain
   echo "################################################"
+  sleep 1
   ./run.sh openmp
   echo "################################################"
+  sleep 1
   ./run.sh opencl
   echo "################################################"
 fi
